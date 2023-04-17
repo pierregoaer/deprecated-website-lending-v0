b01d694b87747363220edf43195828788386dd19
pierregoaer <hello@pierregoaer.com>
2023-04-16 20:54:57 -0400

    mobile navigation and responsivness

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 import '../styles/nav.css';
 
 export default function Nav() {
-	return (
-		<header className="header--container">
-			<div className="header--main">
-				<Link to={'/'} className="header--logo-container">
-					<img className="header--logo" src="/logos/logo_main.png" alt="" />
-				</Link>
-				<ul className="header--main-navigation" data-visible="false">
-					<li>
-						<Link to={'/services'}>Services</Link>
-					</li>
-					<li>
-						<Link to={'/a-propos'}>A propos</Link>
-					</li>
-					<li>
-						<Link to={'/blog'}>Blog</Link>
-					</li>
-				</ul>
-				<Link to={'/contact'} className="header--main-navigation-contact">
-					Contact
-				</Link>
-			</div>
-			<div className="header--mobile-nav-icon-container">
-				<div className="mobile-nav-icon menu-top"></div>
-				<div className="mobile-nav-icon menu-middle"></div>
-				<div className="mobile-nav-icon menu-bottom"></div>
-			</div>
-		</header>
-	);
+    const [mobileNavVisible, setMobileNavVisible] = useState(false)
+
+    function handleDisplayMobileNav() {
+        setMobileNavVisible((prevState => !prevState))
+    }
+
+    return (
+        <header className="header--container">
+            <div className="header--main">
+                <Link to={'/'} className="header--logo-container">
+                    <img className="header--logo" src="/logos/logo_main.png" alt=""/>
+                </Link>
+                <ul className="header--main-navigation" data-visible={mobileNavVisible}>
+                    <li>
+                        <Link to={'/services'}>Services</Link>
+                    </li>
+                    <li>
+                        <Link to={'/a-propos'}>A propos</Link>
+                    </li>
+                    <li>
+                        <Link to={'/blog'}>Blog</Link>
+                    </li>
+                </ul>
+                <Link to={'/contact'} className="btn-primary header--main-navigation-contact">
+                    Contact
+                </Link>
+                <div className="header--mobile-nav-icon-container" data-visible={mobileNavVisible} onClick={handleDisplayMobileNav}>
+                    <div className={`mobile-nav-icon menu-top ${mobileNavVisible ? "active" : ""}`} onClick={handleDisplayMobileNav}></div>
+                    <div className={`mobile-nav-icon menu-middle ${mobileNavVisible ? "active" : ""}`} onClick={handleDisplayMobileNav}></div>
+                    <div className={`mobile-nav-icon menu-bottom ${mobileNavVisible ? "active" : ""}`} onClick={handleDisplayMobileNav}></div>
+                </div>
+            </div>
+        </header>
+    );
 }
