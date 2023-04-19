e8b0fc3e2bf9648f2cdb2b6a55ffb6fe15eb2a7b
pierregoaer <hello@pierregoaer.com>
2023-04-19 09:22:07 -0400

    Adding server side for contact form submissions

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import '../styles/contact.css';
 
 export default function Contact() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+    });
 
-    function handleSubmit(e){
-        e.preventDefault()
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
+    function handleChange(event) {
+        const target = event.target;
+        const {name, value} = target;
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value,
+            };
+        });
     }
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (!formData.name || !formData.email || !formData.message) {
+            console.log("Fields are empty.")
+            return
+        }
+        const dataJSON = JSON.stringify(formData);
+
+        fetch('http://127.0.0.1:5000/contact', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: dataJSON,
+        }).then(response => console.log(response.status));
+    }
+
     return (
         <>
             <section className="contact--container">
@@ -35,6 +72,8 @@ export default function Contact() {
                                 type="text"
                                 placeholder="Nom complet"
                                 name="name"
+                                onChange={handleChange}
+                                value={formData.name}
                             />
                         </div>
                         <div className="contact--form-field">
@@ -43,6 +82,8 @@ export default function Contact() {
                                 type="text"
                                 placeholder="Email"
                                 name="email"
+                                onChange={handleChange}
+                                value={formData.email}
                             />
                         </div>
                         <div className="contact--form-field">
@@ -51,13 +92,17 @@ export default function Contact() {
                                 type="text"
                                 placeholder="Téléphone"
                                 name="phone"
+                                onChange={handleChange}
+                                value={formData.phone}
                             />
                         </div>
                         <div className="contact--form-field">
                             <label htmlFor="message">Votre message*</label>
                             <textarea
-                                placeholder="Votre message"
+                                placeholder="Comment peut-on vous aider ?"
                                 name="message"
+                                onChange={handleChange}
+                                value={formData.message}
                             />
                         </div>
                         <p className="contact--form-disclaimer">Les champs marqués d'un * sont requis.</p>
--- server/app.py
+++ server/app.py
@@ -1,20 +1,73 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify, make_response
+from flask import Flask, request, jsonify, make_response
+from flask_mail import Message, Mail
 from flask_cors import CORS, cross_origin
-import json
-
-from get_data import blog_data
+from datetime import datetime
+import os
+from get_data import blog_data, gsheet_file
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+
+app.config['SECRET_KEY'] = os.environ['APPCONFIGSECRETKEY']
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.environ["EMAIL_SENDER"]
+app.config['MAIL_PASSWORD'] = os.environ["EMAIL_PASSWORD"]
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+# Initialize Mail after config setup
+mail = Mail(app)
+
+# TODO: Select proper origin for DEV or PROD
+cors = CORS(app, resources={r'/*': {'origins': [os.environ['CORS_ORIGIN_DEV']]}})
+
+
+# cors = CORS(app, resources={r'/*': {'origins': [os.environ['CORS_ORIGIN_PROD']]}})
 
 
 @app.route('/get-blogs')
-@cross_origin()
 def get_blogs():
-    print("requesting blogs")
     return jsonify(blog_data), 200
 
 
+@app.route('/contact', methods=['POST'])
+@cross_origin()
+def contact():
+    # get contact form data
+    form_data = request.get_json()
+    name = form_data["name"]
+    email = form_data["email"]
+    phone = form_data["phone"]
+    message = form_data["message"]
+    today = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
+
+    # update google sheet
+    contacts_worksheet = gsheet_file.worksheet("contacts")
+    worksheet_rows = len(contacts_worksheet.get_all_values())
+    new_data = [today, name, email, phone, message]
+    for col in range(1, 6):
+        contacts_worksheet.update_cell(worksheet_rows + 1, col, str(new_data[col - 1]))
+
+    # send email notification
+    html = f"Electricien Nîmes - Nouveau message!<br>" \
+           f"<br>" \
+           f"Date : {today}<br>" \
+           f"Nom : {name}<br>" \
+           f"Email : {email}<br>" \
+           f"Téléphone : {phone}<br>" \
+           f"Message : {message}<br><br>" \
+           f"Voir https://docs.google.com/spreadsheets/d/1PDZtqhsUVgdv83s_uAGr3Z_ElT4_dfMtBocGqSq9SxM/edit#gid=330549784"
+    msg = Message(
+        subject='Electricien Nîmes - Nouveau message!',
+        html=html,
+        sender=('Contact - Electricien Nîmes', app.config['MAIL_USERNAME']),
+        recipients=[os.environ["EMAIL_RECIPIENT_1"], os.environ["EMAIL_RECIPIENT_2"]]
+    )
+    mail.send(msg)
+    return "success", 200
+
+
 if __name__ == "__main__":
+    # TODO: Select proper run for DEV or PROD
     app.run(host="127.0.0.1", debug=True)
+    # app.run()
